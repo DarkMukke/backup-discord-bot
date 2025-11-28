674e0b584407f7b6ad2116645c767a52c4416b85
Mukke <ruben.decleyn@gmail.com>
2025-11-28 13:45:15 +0000

    Fixed the layout spacing and added an attempt to do mobile menu, it works, but it will require an overhaul at some point

--- ui/src/DashboardLayout.jsx
+++ ui/src/DashboardLayout.jsx
@@ -8,22 +8,38 @@ import {
     Toolbar,
     AppBar,
     Typography,
+    IconButton,
 } from '@mui/material';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const drawerWidth = 240;
 
 export default function DashboardLayout({ user }) {
-    const location = useLocation();
-    const path = location.pathname;
-
-    const isAddServer = path === '/' || path === '/add-server';
-    const isManageChannels = path.startsWith('/manage-channels') || path.startsWith('/channels/');
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const handleDrawerToggle = () => setMobileOpen((prev) => !prev);
 
     return (
         <Box sx={{ display: 'flex' }}>
-            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+            <AppBar
+                position="fixed"
+                sx={{
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                    width: { md: `calc(100% - ${drawerWidth}px)` },
+                    ml: { md: `${drawerWidth}px` },
+                }}
+            >
                 <Toolbar>
+                    {/* Hamburger on mobile */}
+                    <IconButton
+                        color="inherit"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { md: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
                     <Typography variant="h6" noWrap component="div">
                         Discord Backup Bot
                     </Typography>
@@ -34,39 +50,74 @@ export default function DashboardLayout({ user }) {
                 </Toolbar>
             </AppBar>
 
-            <Drawer
-                variant="permanent"
-                sx={{
-                    width: drawerWidth,
-                    '& .MuiDrawer-paper': { width: drawerWidth, boxSizing: 'border-box' },
-                }}
+            {/* Nav container for both drawer variants */}
+            <Box
+                component="nav"
+                sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
             >
-                <Toolbar />
-                <Box sx={{ overflow: 'auto' }}>
-                    <List>
-                        <ListItemButton
-                            component={Link}
-                            to="/add-server"
-                            selected={isAddServer}
-                        >
-                            <ListItemText primary="Add Server" />
-                        </ListItemButton>
-                        <ListItemButton
-                            component={Link}
-                            to="/manage-channels"
-                            selected={isManageChannels}
-                        >
-                            <ListItemText primary="Manage Channels" />
-                        </ListItemButton>
-                    </List>
-                </Box>
-            </Drawer>
+                {/* Mobile drawer (temporary) */}
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{ keepMounted: true }}
+                    sx={{
+                        display: { xs: 'block', md: 'none' },
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                >
+                    <Toolbar />
+                    <Box sx={{ overflow: 'auto' }}>
+                        <List>
+                            <ListItemButton component={Link} to="/add-server">
+                                <ListItemText primary="Add Server" />
+                            </ListItemButton>
+                            <ListItemButton component={Link} to="/manage-channels">
+                                <ListItemText primary="Manage Channels" />
+                            </ListItemButton>
+                        </List>
+                    </Box>
+                </Drawer>
 
+                {/* Desktop drawer (permanent) */}
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        display: { xs: 'none', md: 'block' },
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                    open
+                >
+                    <Toolbar />
+                    <Box sx={{ overflow: 'auto' }}>
+                        <List>
+                            <ListItemButton component={Link} to="/add-server">
+                                <ListItemText primary="Add Server" />
+                            </ListItemButton>
+                            <ListItemButton component={Link} to="/manage-channels">
+                                <ListItemText primary="Manage Channels" />
+                            </ListItemButton>
+                        </List>
+                    </Box>
+                </Drawer>
+            </Box>
+
+            {/* Main content */}
             <Box
                 component="main"
-                sx={{ flexGrow: 1, p: 3, marginLeft: `${drawerWidth}px` }}
+                sx={{
+                    flexGrow: 1,
+                    p: 3,
+                    mt: 8, // below AppBar
+                    width: { md: `calc(100% - ${drawerWidth}px)` },
+                }}
             >
-                <Toolbar />
                 <Outlet />
             </Box>
         </Box>
